ab424619920299c98251efc30593b924d8590d85
EliDev7 <91292720+EliDev7@users.noreply.github.com>
2022-10-04 02:15:00 -0300

    Update main.js

--- games/main.js
+++ games/main.js
@@ -49,81 +49,6 @@
 		new Pieza("peon-black", 31, directorioRaiz),
 		new Pieza("peon-black", 32, directorioRaiz),
 	];
-		
-	let drags = "draggable= \"true\" ondragstart=\"drag(event)\" id=\"drag1\" width=\"60\" height=\"60\"";
-	
-	// Creadores:
-	
-	function sAppend(a, b) {
-		return a + b;
-	}
-	
-	function setAttr(field, value) {
-		return " " + field + "= " + value;
-	}
-	
-	function createStyleAttrList() {
-		var attrData;// i.e: color: blue; width: 4px; field: value;
-		for (var i = 0; i < arguments.length; i++) {
-			attrData += arguments[i];
-		}
-		return attrData;// color: blue; width: 4px; field: value;
-	}
-	
-	function createDiv(numId) {
-	var elId = "div" + numId;// ondrop=\"drop(event)\" ondragover=\"allowDrop(event)\"
-	var styleAttrs = "background-color:transparent;width:60px;height:60px;";
-	var attrList = setAttr("id", elId) + setAttr("style", styleAttrs) + setAttr("ondrop", "drop(event)") + setAttr("ondragover", "allowDrop(event)");
-		return "<div" + attrList + "> </div>";
-	}
-	
-	function createTD(numId, color) {
-		return "<td" + setAttr("class", color) + ">" + createDiv(numId++) + "</td>";
-//		return "<td" + setAttr("id", elId) + ">" + createDiv(numId) + "</td>";
-	}
-	
-	function createRow(colorA, colorB, numeroId) {
-		// @numeroId = 1 - inicialmente. Luego: 9, 17, 25, 33, 41, 49, 57.
-		//alert(numeroId);
-		var row = "<tr>";
-		for (var i = 0; i < 8; i++) {
-			if (i % 2 == 0) {
-				row += createTD(numeroId, colorA);
-			} else {
-				row += createTD(numeroId, colorB);
-			}
-			numeroId++;
-		}
-		row += "</tr>";
-		return row;
-	}
-  
-     function createRows() {
-		var tableRows = ["1", "2", "3", "4", "5", "6", "7", "8"];
-		var contador = 1;
-        for(var i = 0; i < 8; i++) {
-			if (i % 2 == 0) {
-				tableRows[i] = createRow("white", "black", contador);
-			} else {
-				tableRows[i] = createRow("black", "white", contador);
-			}
-			contador += 8;
-        }
-        return tableRows;
-      }
-	  
-	  function loadTable() {
-		var chessTable = document.getElementById("chessTab");
-		var tablero = "";
-		var tableRows = createRows();
-		
-		for (var i = 0; i < tableRows.length; i++) {
-			tablero += tableRows[i];
-		}
-		chessTable.innerHTML = tablero;
-		alert("Se ha construido el tablero");
-		addImg();
-	  }
 	  
 	  function addImg() {
 			colocar(blancas, casillasBlancas);
@@ -134,9 +59,15 @@
 		
 		function colocar(piezas, casillas) {
 			let temp;
+			let drags = 'draggable=\"true\" ondragstart=\"drag(event)\"';
+			let imgSize = 'width=\"60\" height=\"60\"';
 			for (let i = 0; i < casillas.length; i++){
+				let imgId = 'id="drag' + casillas[i] + '"';
+				let imgSrc = 'src="' + piezas[i].imagen + '"';
+				
 				temp = document.getElementById("div" + casillas[i]);
-				temp.innerHTML = "<img src=" + piezas[i].imagen + " " + drags + ">";
+				let tempId ="drag" + casillas[i];
+				temp.innerHTML = "<img " + imgId + ' ' + imgSrc + ' ' + imgSize + ' ' + drags + '>';
 			}
 		}
 	  
@@ -153,4 +84,5 @@
 		var data = ev.dataTransfer.getData("text");
 		ev.target.appendChild(document.getElementById(data));
 	}
-	loadTable()
+	loadTable();
+	addImg();
